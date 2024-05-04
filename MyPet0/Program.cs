2ba7b3354f801128852b3c2f48e2a19431c5e267
KimberlyRosaly <01010110.v@gmail.com>
2024-05-03 22:02:53 -0400

    fix syntax errors and make sure there are 'static' keywords with the dictionaries so they can be properly accessed

--- MyPet0/Program.cs
+++ MyPet0/Program.cs
@@ -4,15 +4,15 @@ using System.Threading.Tasks;
 
 class Program
 {
-    Dictionary<string, string> prompts = new Dictionary<string, string>
+    static Dictionary<string, string> prompts = new Dictionary<string, string>
     {
         { "greeting", "WELCOME HOME!" },
         { "introduction", "I AM YOUR PET!" },
         { "instructions", "Please, make sure I don't get too hungry!" },
         { "callToAction", "Revive me? Input 'PLEASE!' and press [ENTER] key" },
         { "outro", "G O O D B Y E !\nI WILL MISS YOU!\nThanks for playing! ❤" }
-    }
-    Dictionary<string, string> hunger = new Dictionary<string, string>
+    };
+    static Dictionary<string, string> hunger = new Dictionary<string, string>
     {
         { "label", "LEVEL OF H U N G E R : " },
         { "meter0", "[ ][ ][ ][ ][ ]" },
@@ -21,8 +21,8 @@ class Program
         { "meter3", "[x][x][x][ ][ ]" },
         { "meter4", "[x][x][x][x][ ]" },
         { "meter5", "[x][x][x][x][x]" }
-    }
-    Dictionary<string, string> pet = new Dictionary<string, string>
+    };
+    static Dictionary<string, string> pet = new Dictionary<string, string>
     {
         { "inhale", @"
       _ _ _ 
@@ -48,8 +48,8 @@ class Program
    |  x   X  |
     \_ _ _ _/
 " }
-    }
-    Dictionary<string, string> petTalk = new Dictionary<string, string>
+    };
+static Dictionary<string, string> petTalk = new Dictionary<string, string>
     {
         { "not hungry", "Ahhh.. I'm not hungry at all!" },
         { "getting hungry", "*Belly grumbles*" },
@@ -57,7 +57,7 @@ class Program
         { "super hungry", "Uh oh.. I'm super hungry now.." },
         { "starving", "I M S-S-STARVING! HALP!" },
         { "death", "O'NOOOO! I M DED!" }
-    }
+    };
 
     static object consoleLock = new object();
     static bool alive = true;
@@ -107,42 +107,42 @@ class Program
                 lock (consoleLock)
                 {
                     Console.SetCursorPosition(0, 6);
-                    Console.Write("LEVEL OF H U N G E R : ");
+                    Console.Write(hunger["label"]);
                     switch (meterIndex)
                     {
                         case 0:
-                            Console.WriteLine(hungerMeter0);
+                            Console.WriteLine(hunger["meter0"]);
                             {
                                 Console.SetCursorPosition(0, 13);
                                 Console.Write(new string(' ', Console.WindowWidth));
                                 Console.SetCursorPosition(0, 13);
-                                SlowPrint("Ahhh.. I'm not hungry at all!", 50);
+                                SlowPrint(petTalk["not hungry"], 50);
                             }
                             meterIndex += 1;
                             break;
                         case 1:
-                            Console.WriteLine(hungerMeter1);
+                            Console.WriteLine(hunger["meter1"]);
                             {
                                 Console.SetCursorPosition(0, 13);
                                 Console.Write(new string(' ', Console.WindowWidth));
                                 Console.SetCursorPosition(0, 13);
-                                SlowPrint("*Belly grumble*", 50);
+                                SlowPrint(petTalk["getting hungry"], 50);
                             }
                             meterIndex += 1;
                             break;
                         case 2:
-                            Console.WriteLine(hungerMeter2);
+                            Console.WriteLine(hunger["meter2"]);
                             {
                                 Console.SetCursorPosition(0, 13);
                                 Console.Write(new string(' ', Console.WindowWidth));
                                 Console.SetCursorPosition(0, 13);
-                                SlowPrint("*Belly grumbles louder*", 50);
+                                SlowPrint(petTalk["getting hungrier"], 50);
                             }
                             meterIndex += 1;
                             break;
                         case 3:
                             Console.ForegroundColor = ConsoleColor.Yellow;
-                            Console.WriteLine(hungerMeter3);
+                            Console.WriteLine(hunger["meter3"]);
                             Console.ResetColor();
                             meterIndex += 1;
                             {
@@ -150,13 +150,13 @@ class Program
                                 Console.Write(new string(' ', Console.WindowWidth));
                                 Console.SetCursorPosition(0, 13);
                                 Console.ForegroundColor = ConsoleColor.Yellow;
-                                SlowPrint("Uh oh..I'm super hungry now..", 50);   
+                                SlowPrint(petTalk["super hungry"], 50);   
                                 Console.ResetColor();
                             }
                             break;
                         case 4:
                             Console.ForegroundColor = ConsoleColor.DarkYellow;
-                            Console.WriteLine(hungerMeter4);
+                            Console.WriteLine(hunger["meter4"]);
                             Console.ResetColor();
                             starving = true;
                             meterIndex += 1;
@@ -165,13 +165,13 @@ class Program
                                 Console.Write(new string(' ', Console.WindowWidth));
                                 Console.SetCursorPosition(0, 13);
                                 Console.ForegroundColor = ConsoleColor.DarkYellow;
-                                SlowPrint("I M SSSSTARVING! HALP!", 50);
+                                SlowPrint(petTalk["starving"], 50);
                                 Console.ResetColor();
                             }
                             break;
                         case 5:
                             Console.ForegroundColor = ConsoleColor.Red;
-                            Console.Write(hungerMeter5);
+                            Console.Write(hunger["meter5"]);
                             Console.ResetColor();
                             alive = !alive;
                             lock (consoleLock)
@@ -180,7 +180,7 @@ class Program
                                 Console.Write(new string(' ', Console.WindowWidth));
                                 Console.SetCursorPosition(0, 13);
                                 Console.ForegroundColor = ConsoleColor.Red;
-                                Console.Write("O'NOOOO! I M DED!");
+                                Console.Write(petTalk["death"]);
                                 Console.ResetColor();
 
                             }
